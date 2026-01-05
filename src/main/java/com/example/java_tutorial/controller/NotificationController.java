71d3f176128fe07631eeab41bac4f06eb42266cd
Wishwell Oklu <wishwell.oklu@etranzact.com.gh>
2026-01-05 15:24:17 +0000

    Update the delete notification method from GET to DELETE

--- src/main/java/com/example/java_tutorial/controller/NotificationController.java
+++ src/main/java/com/example/java_tutorial/controller/NotificationController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -38,7 +39,7 @@ public class NotificationController {
         return ResponseEntity.ok(new ApiResponseDto<>(true, "Notification marked as read successfully", null));
     }
 
-    @GetMapping("/delete/{notificationId}")
+    @DeleteMapping("/delete/{notificationId}")
     public ResponseEntity<ApiResponseDto<NotificationModel>> deleteNotification(@PathVariable String notificationId) {
         notificationService.deleteNotification(notificationId);
         return ResponseEntity.ok(new ApiResponseDto<>(true, "Notification deleted successfully", null));
