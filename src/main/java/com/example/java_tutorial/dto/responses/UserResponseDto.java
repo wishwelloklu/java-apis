380807d4d8f859e535189bff10bc7dcdc50b3177
Wishwell Oklu <wishwell.oklu@etranzact.com.gh>
2026-02-13 12:31:46 +0000

    feat: Add Lombok `@Builder` and `@NoArgsConstructor` to `UserResponseDto` and include MapStruct processor in `pom.xml`.

--- src/main/java/com/example/java_tutorial/dto/responses/UserResponseDto.java
+++ src/main/java/com/example/java_tutorial/dto/responses/UserResponseDto.java
@@ -1,34 +1,23 @@
 package com.example.java_tutorial.dto.responses;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.google.auto.value.AutoValue.Builder;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserResponseDto {
-
     private Long id;
     private String firstName;
     private String lastName;
     private String phoneNumber;
     private String email;
     private String role;
-
-    public UserResponseDto(Long id, String firstName, String lastName, String email, String phoneNumber) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public UserResponseDto(Long id, String firstName, String lastName, String email, String phoneNumber, String role) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-        this.role = role;
-    }
 
 }
